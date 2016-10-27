87e4f381601b09d949335afb288b951dc8ff8244
Richard Trebichavský <richard.trebichavsky@gmail.com>
2016-10-27 20:56:42 +0200

    [minor] Entities and DAOs: Updated comments and fixed @author tag

--- fleet-management/src/main/java/cz/fi/muni/pa165/dao/InspectionIntervalDao.java
+++ fleet-management/src/main/java/cz/fi/muni/pa165/dao/InspectionIntervalDao.java
@@ -3,8 +3,7 @@ package cz.fi.muni.pa165.dao;
 import cz.fi.muni.pa165.entity.InspectionInterval;
 
 /**
- * Created by MBalicky on 23/10/2016.
- * @Author Michal Balický
+ * @author Michal Balický
  */
 public class InspectionIntervalDao extends JpaDao<InspectionInterval, Long> {
 }
