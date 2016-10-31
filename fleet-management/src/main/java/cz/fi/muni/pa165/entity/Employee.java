df1613a74f7b5294cbfa625778e03bbc37e8fa2e
Richard Trebichavský <richard.trebichavsky@gmail.com>
2016-10-31 20:25:32 +0100

    Entities: Updated and added equals(), hashCode() and toString() methods

--- fleet-management/src/main/java/cz/fi/muni/pa165/entity/Employee.java
+++ fleet-management/src/main/java/cz/fi/muni/pa165/entity/Employee.java
@@ -43,4 +43,29 @@ public class Employee {
     public void setSurname(String surname) {
         this.surname = surname;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+
+        return id != null ? id.equals(employee.id) : employee.id == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
 }
--- fleet-management/src/main/java/cz/fi/muni/pa165/entity/InspectionInterval.java
+++ fleet-management/src/main/java/cz/fi/muni/pa165/entity/InspectionInterval.java
@@ -22,29 +22,53 @@ public class InspectionInterval {
     @Column(nullable = false)
     private int days;
 
-    public InspectionInterval(String name, int interval){
+    public InspectionInterval(String name, int interval) {
         this.name = name;
         this.days = interval;
     }
 
-    public Long getId(){
+    public Long getId() {
         return this.id;
     }
 
-    public String getName(){
+    public String getName() {
         return this.name;
     }
 
-    public int getDays(){
+    public int getDays() {
         return this.days;
     }
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
 
-    public void setDays(int days){
+    public void setDays(int days) {
         this.days = days;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        InspectionInterval that = (InspectionInterval) o;
+
+        return id != null ? id.equals(that.id) : that.id == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "InspectionInterval{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", days=" + days +
+                '}';
+    }
 }
