d3907f18dd8f27e55550732aff8e25d6131b0a11
Martin Schmidt <m.schmidt@email.cz>
2016-11-13 15:51:04 +0100

    made multimodule project, added missing relationships to entities

---
+++ fleet-management-persistence/src/main/java/cz/fi/muni/pa165/entity/Employee.java
@@ -0,0 +1,110 @@
+package cz.fi.muni.pa165.entity;
+
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Jozef Krcho
+ */
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false)
+    private String name;
+
+    @Column(nullable=false)
+    private String surname;
+
+
+    @ManyToMany
+    Set<VehicleCategory> vehicleCategories = new HashSet<>();
+
+    @OneToMany(mappedBy = "employee")
+    private Set<Journey> journeys = new HashSet<>();
+
+    /**
+     * All persistent classes must have a default constructor (which can be non-public)
+     * so that Hibernate can instantiate them using Constructor.newInstance().
+     */
+    protected Employee() {
+    }
+
+    /**
+     * @param name    name of the Employee
+     * @param surname surname of the Employee
+     */
+    public Employee(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Set<VehicleCategory> getVehicleCategories() {
+        return vehicleCategories;
+    }
+
+    public void setVehicleCategories(Set<VehicleCategory> vehicleCategories) {
+        this.vehicleCategories = vehicleCategories;
+    }
+
+    public Set<Journey> getJourneys() {
+        return journeys;
+    }
+
+    public void setJourneys(Set<Journey> journeys) {
+        this.journeys = journeys;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || (!(o instanceof Employee))) return false;
+
+        Employee employee = (Employee) o;
+
+        if (name != null ? !name.equals(employee.getName()) : employee.getName() != null) return false;
+        return surname != null ? surname.equals(employee.getSurname()) : employee.getSurname() == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (surname != null ? surname.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
+}
