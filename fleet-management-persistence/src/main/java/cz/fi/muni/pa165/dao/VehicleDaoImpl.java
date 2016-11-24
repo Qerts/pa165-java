5da3ec2e662b02dc528ba481aad124369ced6529
Qerts <michal.balicky@gmail.com>
2016-11-24 19:26:59 +0100

    WIP findVehiclesAvailable method and test added, test crashing;

--- fleet-management-persistence/src/main/java/cz/fi/muni/pa165/dao/VehicleDaoImpl.java
+++ fleet-management-persistence/src/main/java/cz/fi/muni/pa165/dao/VehicleDaoImpl.java
@@ -7,6 +7,9 @@ import cz.fi.muni.pa165.entity.Vehicle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.util.Iterator;
 import java.util.List;
 
@@ -15,4 +18,30 @@ import java.util.List;
  */
 @Repository
 public class VehicleDaoImpl extends JpaDao<Vehicle, Long> implements VehicleDao {
+
+
+    @PersistenceContext
+    private EntityManager em;
+
+
+    @Override
+    public List<Vehicle> findVehiclesAvailable(long employeeId) {
+        Query query = this.em.createQuery(
+                "SELECT v " +
+                    "FROM Vehicle v " +
+                    "INNER JOIN v.vehicleCategory vc " +
+                    "INNER JOIN vc.employees e" +
+                    "WHERE e.id = :employeeId",
+                Journey.class);
+
+        query.setParameter("employeeId", employeeId);
+
+        List<Vehicle> result = query.getResultList();
+
+        return result;
+    }
+    //vozidla
+    //join employees
+    //kde id categorie je stejne
+    //join journey
 }
