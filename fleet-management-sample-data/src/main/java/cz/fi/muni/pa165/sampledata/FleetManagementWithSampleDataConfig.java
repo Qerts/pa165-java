836c200a7b4f47d4cc94320a18addee7ce8c4fb9
Unknown <jozefekrcho@gmail.com>
2016-12-07 10:34:19 +0100

    finishing sample data test and config

--- fleet-management-sample-data/src/main/java/cz/fi/muni/pa165/sampledata/FleetManagementWithSampleDataConfig.java
+++ fleet-management-sample-data/src/main/java/cz/fi/muni/pa165/sampledata/FleetManagementWithSampleDataConfig.java
@@ -1,9 +1,16 @@
 package cz.fi.muni.pa165.sampledata;
 
 import cz.fi.muni.pa165.config.ServiceConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import java.io.IOException;
 
 /**
  * @author Jozef Krcho
@@ -13,4 +20,14 @@ import org.springframework.context.annotation.Import;
 @ComponentScan(basePackageClasses = {SampleDataLoadingFacadeImpl.class})
 public class FleetManagementWithSampleDataConfig {
 
+    final static Logger log = LoggerFactory.getLogger(FleetManagementWithSampleDataConfig.class);
+
+    @Inject
+    SampleDataLoadingFacade sampleDataLoadingFacade;
+
+    @PostConstruct
+    public void dataLoading() throws IOException {
+        log.debug("dataLoading()");
+        sampleDataLoadingFacade.loadData();
+    }
 }
